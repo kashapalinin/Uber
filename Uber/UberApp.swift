4a79d7eab95cabb512a5f2afdd9b4f7c695a85d0
pavel610 <78311696+pavel610@users.noreply.github.com>
2024-03-03 09:48:18 +0300

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Павел Калинин on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
